9e5f7460e4fa13b927a1356f7ca090e2b26529fc
Devan Allen McGranahan <devan.mcgranahan@gmail.com>
2021-02-18 09:40:52 -0700

    robust imputation update

--- r script/imputation.R
+++ r script/imputation.R
@@ -14,21 +14,30 @@ AnalysisData <- read_csv(paste0(getwd(), "/data/AnalysisDataKY.csv"))
         md.pattern() 
 
   # Calculate imputed datasets
-    imp <- AnalysisData %>% 
-            select(-LAI) %>%
-                mutate(across(plot:array, ~as.character(.))) %>%
-                 mice(m=500, seed = 23109, print=F)
+    imp_sc <- AnalysisData %>% 
+            select(-LAI, -JD) %>%
+            mutate_at(vars(AirTemp:tHa), ~as.numeric(scale(., center=F))) %>%
+                      mutate(across(location:array, as.factor)) %>%
+                       mice(m=5, seed = 23109, print=F)
     
     imp_raw <- 
-      complete(imp) %>%
-              as_tibble() %>% 
-                mutate(across(location:array, as.factor)) 
+      complete(imp, 'long') %>%
+      as_tibble() %>%
+      unite("TreeID", c(location, block, pasture,
+                        year, plot, array), sep = ".") %>%
+      select(-patch, -.id, -.imp, -FireCode) %>%
+      pivot_longer(names_to = "response", 
+                   values_to = "values", 
+                   -TreeID) %>%
+      group_by(TreeID, response) %>%
+      summarize(value = median(values)) %>%
+      ungroup() %>%
+      pivot_wider(names_from = response, 
+                  values_from = value) %>%
+      separate(TreeID, c("location", "block", "pasture",
+                         "year", "plot", "array")) %>% 
+      mutate(across(location:array, as.factor))  
     
-    imp_sc <- 
-      imp_raw %>%
-       mutate_at(vars(AirTemp:tHa), ~as.numeric(scale(., center=F)))
-  
+
     # save(imp_raw, file = paste0(FilePath, '/r objects/imp_raw.Rdata'))
     # save(imp_sc, file = paste0(FilePath, '/r objects/imp_sc.Rdata'))
-
-    
